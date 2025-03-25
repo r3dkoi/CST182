96085accc36cab558f91f3c6805eb0ab364c5fd2
r3dkoi <188728180+r3dkoi@users.noreply.github.com>
2025-03-25 18:23:08 +0800

    Draft 1 of add_mark

---
+++ Student Marks/functions.py
@@ -0,0 +1,12 @@
+#Functions for student marks
+
+def add_mark():
+    with open("student_marks.txt", "a") as f: #Allows function to open file and then close it immediately when finished
+        mark = int(input("Please input a mark you wish to add to the student."))
+        while True:
+            try:
+                if mark < 0:
+                    print("Please input a mark that is non-negative.")
+            except ValueError:
+                print("Please input only numbers, not letters or any other characters.")
+            
