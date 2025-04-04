1a766e496086b7a9ccf82c6a7f4927d10e913aa6
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-04 17:32:05 +0800

    Added calculation and output for the average number of people per class in task_1_Pandas.py, improved comments, and refined the display of the number of people in each class for better clarity.

--- Data Analysis/task_1_Pandas.py
+++ Data Analysis/task_1_Pandas.py
@@ -14,14 +14,33 @@ male_count = df['Sex'] == 'male'
 female_count = df['Sex'] == 'female'
 ratio_m_and_f = round((male_count.sum() / female_count.sum()),1)
 
-#Finding the number of people per class [Groupby() method practice]
-people_per_class = df.groupby('PClass').size() 
+#Finding the number of people per class [1, 2, 3 class] [Groupby() method practice]
+people_per_class = df.groupby('Pclass').size()
 
 
-print() # 4. aggregates per class (note: groupby() method - mean,max,min,count)
+#Finding the mean, max, min, and count of people per class [Groupby() method - mean,max,min,count practice]
+    # Calculate the average number of people per class
+average_per_class = df.groupby('Pclass').size().mean()
+
+# Calculate the count of people in each class
+
+
+print(f"\n4. The average amount of people per class was: {average_per_class:.1f}")
+
+
+
+
+
+
+
+
 print() # 5. aggregates per class and gender (note: groupby() method - mean,max,min,count)
 
 #Display section
 print(f"\n1. The number of people aboard was {len(df)}") 
 print(f"\n2. The ratio of male to females passengers on board were: {ratio_m_and_f}")
-print(f"\n3. The number of people per class were: {people_per_class}") 
+print(f"\n3. Number of people in each class: ")
+for pclass, count in people_per_class.items():
+    print(f"   Class {pclass} = {count} people")
+
+
