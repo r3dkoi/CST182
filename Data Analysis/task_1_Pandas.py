e3917ea68d5f2656ceeb0dfaeb46efd281ad49d4
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-04 16:56:13 +0800

    Reorganized output statements in task_1_Pandas.py to improve clarity, added display of the number of people per class, and retained previous calculations for better data presentation.

--- Data Analysis/task_1_Pandas.py
+++ Data Analysis/task_1_Pandas.py
@@ -7,8 +7,6 @@ import pandas as pd
 df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
 print(df.head()) #debug statement + checking data we're working with
 
-#The no. of people aboard
-print(f"\n1. The number of people aboard was {len(df)}") 
 
 #Finding the ratio of male to females  [Selection of Data]
 """This filters specifically the males and females from the dataset"""
@@ -16,9 +14,14 @@ male_count = df['Sex'] == 'male'
 female_count = df['Sex'] == 'female'
 ratio_m_and_f = round((male_count.sum() / female_count.sum()),1)
 
-print(f"\n2. The ratio of male to females passengers on board were: {ratio_m_and_f}")
+#Finding the number of people per class [Groupby() method practice]
+people_per_class = df.groupby('PClass').size() 
 
 
-print() # 3. number of people per class (note: groupby() method)
 print() # 4. aggregates per class (note: groupby() method - mean,max,min,count)
 print() # 5. aggregates per class and gender (note: groupby() method - mean,max,min,count)
+
+#Display section
+print(f"\n1. The number of people aboard was {len(df)}") 
+print(f"\n2. The ratio of male to females passengers on board were: {ratio_m_and_f}")
+print(f"\n3. The number of people per class were: {people_per_class}") 
