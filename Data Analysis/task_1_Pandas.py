79cba265b4bdd3d609f6a7f932da4805ff820cb3
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-04 16:31:54 +0800

    Updated task_1_Pandas.py to print DataFrame head and added output for the number of people aboard.

--- Data Analysis/task_1_Pandas.py
+++ Data Analysis/task_1_Pandas.py
@@ -5,9 +5,10 @@ import pandas as pd
 
 # TODO: read the csv into a DataFrame and then fill in each print statement
 df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
-df.head() #debug statement + checking data we're working with
+print(df.head()) #debug statement + checking data we're working with
 
-print() # 1. number of people aboard (basic aggregate or length)
+print(f"\n1. The number of people aboard was {len(df)}") # 1. number of people aboard (basic aggregate or length)
+
 print() # 2. ratio of male to females (selecting data)
 print() # 3. number of people per class (note: groupby() method)
 print() # 4. aggregates per class (note: groupby() method - mean,max,min,count)
