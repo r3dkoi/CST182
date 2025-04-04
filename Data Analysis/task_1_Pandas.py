dc685dcc0a5988f2e9a628dc8784c3feddd0ba45
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-04 16:37:32 +0800

    Enhanced task_1_Pandas.py by adding the calculation and output of the male-to-female passenger ratio, along with improved comments for clarity.

--- Data Analysis/task_1_Pandas.py
+++ Data Analysis/task_1_Pandas.py
@@ -7,9 +7,18 @@ import pandas as pd
 df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
 print(df.head()) #debug statement + checking data we're working with
 
-print(f"\n1. The number of people aboard was {len(df)}") # 1. number of people aboard (basic aggregate or length)
+#The no. of people aboard
+print(f"\n1. The number of people aboard was {len(df)}") 
 
-print() # 2. ratio of male to females (selecting data)
+#Finding the ratio of male to females  [Selection of Data practice]
+"""This filters specifically the males and females from the dataset"""
+male_count = df['Sex'] == 'male' 
+female_count = df['Sex'] == 'female'
+ratio_m_and_f = male_count.sum() / female_count.sum()
+
+print(f"\n2. The ratio of male to females passengers on board were: {ratio_m_and_f}")
+
+
 print() # 3. number of people per class (note: groupby() method)
 print() # 4. aggregates per class (note: groupby() method - mean,max,min,count)
 print() # 5. aggregates per class and gender (note: groupby() method - mean,max,min,count)
