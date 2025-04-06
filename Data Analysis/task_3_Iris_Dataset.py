9132ac9ec3482ed360f9f3d9b0d994464fe6a11e
r3dkoi <188728180+r3dkoi@users.noreply.github.com.>
2025-04-06 09:29:02 +0800

    Added a new script task_3_Iris_Dataset.py to load and display the Iris dataset using pandas, including a debug statement to verify data loading.

---
+++ Data Analysis/task_3_Iris_Dataset.py
@@ -0,0 +1,8 @@
+# Student Name: Kyla Sofocado
+# Student Number: 1034672
+
+import pandas as pd
+
+df = pd.read_csv("https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv")
+
+print(df.head()) #Debug statement to show if the data is being read correctly.
